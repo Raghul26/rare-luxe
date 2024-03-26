6faefba2fee9a8fe11cd95cfbc88d423e522a86b
raghul <raghulheart26@gmail.com>
2024-03-26 11:01:53 +0530

    productpromos modified

--- src/components/Productpromos/Productpromos.js
+++ src/components/Productpromos/Productpromos.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import "../assets/style/Productpromos.css"
 import videos from "../assets/images/video2.mp4"
-import img from "../assets/images/favourite.png"
+import loadinglogo from "../assets/images/logo.png"
 import { Link } from 'react-router-dom'
 
 const Productpromos = () => {
@@ -13,45 +13,29 @@ const Productpromos = () => {
       console.log("loaded");
     }, 1200);
   }, [])
-  // const companydata = [
-  //   {
-  //     id: "0",
-  //     image: img
-  //   },
-  //   {
-  //     id: "1",
-  //     image: img
-  //   },
-  //   {
-  //     id: "2",
-  //     image: img
-  //   },
-  //   {
-  //     id: "3",
-  //     image: img
-  //   }
-  // ]
   return (
     <>
-      <div className='productvideocontainer'>
-        <div className='productvideocontant'>
-          <video loop autoPlay controls>
-            <source src={videos} type="video/mp4" />
-          </video>
-          <div className='productvideocontantbtn'>
-            <button><Link to={"/signup"} className='productvideocontantbtnlink'>Signup</Link></button>
+      {
+        loading ? <>
+
+          <div className='loading'>
+            <img src={loadinglogo} className='loadinglogo' alt='loading' />
           </div>
-        </div>
-      </div>
-      <div>
-        {/* {
-          companydata.map((item, index) => (
-            <div key={index} className='companydata'>
-              <img src={item.image} alt='' width="90px" height="90px" />
+        </> : <>
+          <div className='productvideocontainer'>
+            <div className='productvideocontant'>
+              <video loop autoPlay>
+                <source src={videos} type="video/mp4" />
+              </video>
+              {/* <div className='productvideocontantbtn'>
+                <button><Link to={"/signup"} className='productvideocontantbtnlink'>Signup</Link></button>
+              </div> */}
             </div>
-          ))
-        } */}
-      </div>
+          </div>
+          <div>
+          </div>
+        </>
+      }
     </>
   )
 }
