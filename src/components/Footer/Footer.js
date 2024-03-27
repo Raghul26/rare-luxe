76acdf45620539c6a205686414e46a0e25243c9f
raghul <raghulheart26@gmail.com>
2024-03-27 15:50:35 +0530

    footer modified

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,11 @@ import React from 'react'
 import "../assets/style/Footer.css"
 import { useState, useEffect } from 'react'
 import logo from "../assets/images/logo.png"
+import followlog1 from "../assets/images/instagram.png"
+import followlog2 from "../assets/images/whatsapp.png"
+import followlog3 from "../assets/images/facebook.png"
+import followlog4 from "../assets/images/twitterx.png"
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
 
@@ -11,7 +16,7 @@ const Footer = () => {
         const year = new Date().getFullYear()
         console.log(year, "year");
         setyear(year)
-    }, [])
+    }, [year])
 
     return (
         <div className='footercontainer'>
@@ -48,18 +53,23 @@ const Footer = () => {
                         </div>
                         <div className='footemenucol'>
                             <p className='footemenucoltitle'>Follow us</p>
-                            <p>Who We Are</p>
-                            <p>Join Our TeamTerms & Conditions</p>
-                            <p> We Respect Your Privacy</p>
-                            <p>Fees & Payments</p>
-                            <p>Returns & Refunds Policy</p>
-                            <p>Promotions Terms & Conditions</p>
+                            <p>Instagram</p>
+                            <p>Facebook</p>
+                            <p>Twitterx</p>
                         </div>
                     </div>
                     <div className='footercompanyrights'>
-                        <p>Copyright @{year} All rights reserved</p>
+                        <div>
+                            <p>Copyright @{year} All rights reserved.</p>
+                        </div>
+                        <div className='footerfollowlog'>
+                            <img src={followlog1} alt='logo' />
+                            <img src={followlog2} alt='logo' />
+                            <img src={followlog3} alt='logo' />
+                            <img src={followlog4} alt='logo' />
+                        </div>
                         <div className='footerlogo'>
-                            <img src={logo} alt='rare-luxe' />
+                            <Link className='footerlogolink' to={'/'}><img src={logo} alt='rare-luxe' /></Link>
                         </div>
                     </div>
                 </div>
