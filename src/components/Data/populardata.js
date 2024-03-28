67e5375f39eaf53bbff175b9ffe288c0e355c2ea
raghul <raghulheart26@gmail.com>
2024-03-28 13:07:21 +0530

    populardata modifed (image added)

--- src/components/Data/populardata.js
+++ src/components/Data/populardata.js
@@ -1,20 +1,22 @@
-import product from "../assets/images/2.jpg"
+import product2 from "../assets/images/2.jpg";
+import product1 from "../assets/images/1.jpg"
+
 export default [
     {
         id: "0",
-        image: product,
+        image: product2,
         name: "EMPORIO ARMANI",
         sublist: "Travel Essential Blended Regular Fit Blazer"
     },
     {
         id: "1",
-        image: product,
+        image: product2,
         name: "ISABEL MARANT",
         sublist: "Zelikia Crew-Neck T-Shirt"
     },
     {
         id: "2",
-        image: product,
+        image: product1,
         name: "KATE SPADE",
         sublist: "Plaid Tie-Waist Coat"
     }
