893e98fecdbc769d0eccba9ef6c74d7da07bfe26
raghul <raghulheart26@gmail.com>
2024-03-29 14:05:32 +0530

    success page added

---
+++ src/components/SuccessScreen/SuccessScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../assets/style/SuccessScreen.css"
+
+const SuccessScreen = () => {
+  return (
+    <div className='successcontainer'>
+      SuccessScreen
+    </div>
+  )
+}
+
+export default SuccessScreen
