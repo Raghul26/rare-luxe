966178c827b4e668c6162e18aebe3538292fa846
raghul26 <raghulheart26@gmail.com>
2024-04-05 18:20:24 +0530

    accountscreen and faviourt screen modified

--- src/components/ErrorScreen/ErrorScreen.js
+++ src/components/ErrorScreen/ErrorScreen.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import empty from "../assets/images/error.png"
+import { Link } from 'react-router-dom';
+import "../assets/style/FaviourtScreen.css"
 
 const ErrorScreen = () => {
   return (
-    <div style={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: '100%',
-      height: '100vh'
-    }}>
-      <p>Oops, we can't find the page you are looking for...</p>
+    <div className='Cartcontainer'>
+      <div className='Cartcontant favouriteproudctno'>
+        <p>Oops, we can't find the page you are looking for...</p>
+        <div className='Cartemptyimage favouriteproudctno'>
+          <img src={empty} alt='empty' />
+        </div>
+        <div className='Cartshopbutton favouriteproudctno'>
+          <Link to={"/auth"} className='Cartshoplinkbutton'>Back To Home</Link>
+        </div>
+      </div>
     </div>
   )
 }
